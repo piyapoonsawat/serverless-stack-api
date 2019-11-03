015dedaaeaa2288b1ed5eb9e18fd739c8abf2ee1
Piya Poonsawat <piyapoonsawat@yahoo.com>
2019-11-03 16:21:00 +0700

    Adding our Serverless API

---
+++ delete.js
@@ -0,0 +1,17 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+export async function main(event, context) {
+  const params = {
+    TableName: process.env.tableName,
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id
+    }
+  };
+  try {
+    await dynamoDbLib.call("delete", params);
+    return success({ status: false });
+    } catch (e) {
+    return failure({ status: false });
+    }
+  }
